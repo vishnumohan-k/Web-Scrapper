3d1f53814b57d72402cdc812558b0244d385bf88
vishnumohan-k <vishnumohan15@gmail.com>
2016-03-15 15:28:08 +0530

    my first commit

--- Login.php
+++ Login.php
@@ -5,6 +5,8 @@ $rslt=mysqli_query($con,$query);
    if(! $rslt ) 
    {
       die('Could not get data: ' . mysql_error());
+      //header("Location: ToLogin.php");
+
    } 
    while($row=mysqli_fetch_row($rslt)) 
    {
@@ -12,7 +14,8 @@ $rslt=mysqli_query($con,$query);
 				$_SESSION['username']=$_POST['email'];
 				//$_SESSION['user_id']=$row[0];
 				session_write_close ();
+            header("Location: UserLogin.php");
 	}  
 mysqli_close($con);
-header("Location: home.php");
+//header("Location: home.php");
 ?>
--- isRegisterd.php
+++ isRegisterd.php
@@ -4,14 +4,15 @@ echo "<div class='container'><div class='row'>";
 session_start();
 	if(!isset($_SESSION['username']))
 		{
-		// "<a href='Login.html'><i class='glyphicon glyphicon-earphone'></i>Login</a>&nbsp|&nbsp<a href='Register.html'>Register</a>";
+		 "<a href='Login.html'><i class='glyphicon glyphicon-earphone'></i>Login</a>&nbsp|&nbsp<a href='Register.html'>Register</a>";
 		}
 		else
 		{
+		echo "<strong>Welcome </strong>";
 		echo "<a href='Account.php'>";
-		echo $_SESSION['username'];
-		echo "&nbsp|&nbsp</a><a href='Wishlist.html'>Wishlist</a>&nbsp|&nbsp";
-		echo "<a href='Logout.php' style='text-decoration:none'><i class='glyphicon glyphicon-off'></i>Log out</a>";
+		echo "<strong>".$_SESSION['username']."</strong>";
+		//echo "&nbsp|&nbsp</a><a href='Wishlist.html'>Wishlist</a>&nbsp|&nbsp";
+		//echo "&nbsp <a href='Logout.php' style='text-decoration:none'><i class='glyphicon glyphicon-off'></i> Log out</a>";
 		}
 	session_write_close ();
 
