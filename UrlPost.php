8e7306063d8b0bd778c013003708b3846dce3b67
vishnumohan-k <vishnumohan15@gmail.com>
2016-03-17 15:24:18 +0530

    3.24pm

---
+++ UrlPost.php
@@ -0,0 +1,10 @@
+<?php
+include ("dbconnect.php");
+$url=$_POST['URL'];
+$range=$_POST['range'];
+$cat=$_POST['catagory'];
+/*echo $url."<br>";
+echo $range."<br>";
+echo $cat."<br>";*/
+
+?>
---
+++ addNewProduct.php
@@ -0,0 +1,5 @@
+<?php
+include 'userPage.php';
+include 'addNewProduct.html';
+
+?>
