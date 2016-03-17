7b294ce08fd8496a0c1dfa03d8445b8ee33d1c23
vishnumohan-k <vishnumohan15@gmail.com>
2016-03-17 22:08:10 +0530

    Adding to wishlist table- 10.07 pm

--- UrlPost.php
+++ UrlPost.php
@@ -11,22 +11,35 @@ echo $newCat;
 if($newCat)
 {
 	//echo "New catagory is added";
-	                session_start();
-                    $query="select user_id from user where email = '".$_SESSION['username']."'";
-                    session_write_close ();    
-                    $rslt=mysqli_query($con,$query);
-                    if(! $rslt ) 
-                    {
-                        die('Could not get data: ' . mysql_error());
-                    }     
+	            session_start();
+                $query="select user_id from user where email = '".$_SESSION['username']."'";
+                session_write_close ();    
+                $rslt=mysqli_query($con,$query);
+                if(! $rslt ) 
+                {
+                    die('Could not get data: ' . mysql_error());
+                }     
                     
-                    while($row=mysqli_fetch_row($rslt)) 
-                    {
+                while($row=mysqli_fetch_row($rslt)) 
+                {
                         
-                        $user=$row[0];
-                    }  
+                    $user=$row[0];
+                }  
                     //echo $user;
-                $query=mysqli_query($con,"INSERT INTO wishlist(user_id_wishlist,wishlist_name) VALUES ('$user','$newCat')");
+              	$query=mysqli_query($con,"INSERT INTO wishlist(user_id_wishlist,wishlist_name) VALUES ('$user','$newCat')");
+              	$query1="select wishlist_id from wishlist where wishlist_name = '".$newCat."'";
+              	$rslt=mysqli_query($con,$query1);
+              	if(! $rslt ) 
+                {
+                        die('Could not get data: ' . mysql_error());
+                }     
+                    
+                while($row=mysqli_fetch_row($rslt)) 
+                {
+                        
+                        $wishlist_id=$row[0];
+                }  
+                //echo "<br>".$wishlist_id;
 }
 
 ?>
