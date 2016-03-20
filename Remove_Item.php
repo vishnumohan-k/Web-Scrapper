12985f34de77c8e1cb96cf83ea1d5fca6a0b80ed
sameesh-s <sameesh.fisat@gmail.com>
2016-03-20 22:47:56 +0530

    deletion successfull

--- Remove_Item.php
+++ Remove_Item.php
@@ -1,38 +1,19 @@
 <?php
-
-$url_id = $_GET['Url_id'];
-echo "URL_ID:".$url_id;
 include 'dbconnect.php';
-/*$query="delete from URL_history where url_id_hist = '".$url_id."'";
-$rslt=mysqli_query($con,$query);
-if(! $rslt ) 
-{
-    die('Could not get data: ' . mysql_error());
-}     
-                    
-while($row=mysqli_fetch_row($rslt)) 
-{}  
+$url_id = $_GET['Url_id'];
+//echo "URL_ID:".$url_id;
 
-$query="delete from Wish_URL where url_id = '".$url_id."'";
+$query="delete from URL_history where url_id_hist = '".$url_id."'";
 $rslt=mysqli_query($con,$query);
-if(! $rslt ) 
-{
-    die('Could not get data: ' . mysql_error());
-}     
-                    
-while($row=mysqli_fetch_row($rslt)) 
-{}    
+ 
+
+$query="delete from Wish_Url where url_id = '".$url_id."'";
+$rslt=mysqli_query($con,$query);
+   
 
 $query="delete from URL where url_id = '".$url_id."'";
 $rslt=mysqli_query($con,$query);
-if(! $rslt ) 
-{
-    die('Could not get data: ' . mysql_error());
-}     
-                    
-while($row=mysqli_fetch_row($rslt)) 
-{}    
-
+//echo "delete suceesful";
 header("Location: Wishlist.php");
-*/
+
 ?>
